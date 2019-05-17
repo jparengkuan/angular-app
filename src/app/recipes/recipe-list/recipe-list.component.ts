77809967a52f1e1f28aebe21b252cecde4e995b2
FJ Parengkuan <jim.parengkuan@gmail.com>
2019-05-17 02:34:31 +0200

    Added several methods of databinding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -7,11 +7,16 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   // Een lege recepten array van het type Recipe (model)
   recipes: Recipe[] = [
     new Recipe(
       'Lasagne',
+      'een klassiek en eenvoudig ovenpastagerecht',
+      'https://i0.wp.com/marielleindekeuken.nl/wp-content/uploads/2018/03/Lasagne.jpg'),
+
+    new Recipe(
+      'Nog een Lasagne',
       'een klassiek en eenvoudig ovenpastagerecht',
       'https://i0.wp.com/marielleindekeuken.nl/wp-content/uploads/2018/03/Lasagne.jpg')
   ];
@@ -21,4 +26,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  OnRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
